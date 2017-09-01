36466855a5b15d50a36302bbafb428a8c902ac0f
ij2872 <ijaimes2872@gmail.com>
2017-09-01 12:27:53 -0500

    Add project files.

---
+++ FakeNetBank/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FakeNetBank.Startup))]
+namespace FakeNetBank
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
